6330b39f722593929f164fc31d3313fc9a61ffb9
Bharat Mazire <mazire.bharat@gmail.com>
2018-05-10 13:57:24 +0530

    Update GuessRandomNumber.py

--- SimpleGames/GuessRandomNumber.py
+++ SimpleGames/GuessRandomNumber.py
@@ -4,20 +4,22 @@ def main():
   print ("RANDOM NUMBER GUESS !!")
   
   value = rm.randint(1,100)
-  chances = 10
+  chances = 5
   flag = 0
   
   while chances != 0:
     print ("Remaining Chances !!!!",chances)
-    number = int(input("Enter number : "))
+    number = int(input("Enter number :"))
+    if (number - value  > 20 or number - value  < -20):
+      print ("Going Too Far..")
     if value == number:
       print ("Well Done...")
       flag = 1
       break
     elif value < number:
-      print ("Value Smaller than you enterd!!")
+      print ("Value Smaller than have you enterd!!")
     elif value > number:
-      print ("Value Greater than you enterd!!")
+      print ("Value Greater than have you enterd!!")
     chances = chances - 1
   
   if flag == 0:
