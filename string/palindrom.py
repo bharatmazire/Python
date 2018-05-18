0ef123c70c37e129360a0a945ad4d8df157382a5
Bharat Mazire <mazire.bharat@gmail.com>
2018-05-18 18:41:57 +0530

    Update palindrom.py

--- string/palindrom.py
+++ string/palindrom.py
@@ -2,9 +2,10 @@
 
 # problem statement : To check wheather given string is palindrom or not. 
 
-s = input("Enter String")
+s = input("Enter String")			# accepting string from user
+
 def chk(s):
-	if s == s[::-1]:
+	if s == s[::-1]:			# comparision between string(s) and reverse of string (s[::-1]) {string slicing}
 		return s+" is palindrome"
 	else:
 		return s+" is not palindrome"
