fa397da404b6b44a84a401b31801f94c6b6a6846
Bharat Mazire <mazire.bharat@gmail.com>
2018-05-18 18:40:30 +0530

    Update palindrom.py

--- string/palindrom.py
+++ string/palindrom.py
@@ -1,13 +1,11 @@
-s = input()
+#!/usr/bin/python
+
+# problem statement : To check wheather given string is palindrom or not. 
+
+s = input("Enter String")
 def chk(s):
 	if s == s[::-1]:
 		return s+" is palindrome"
 	else:
 		return s+" is not palindrome"
 print(chk(s))
-
-# i = 0
-# j = -1
-# while (x[i] == x[j] and i<int(len(x)/2)):
-# 	i+=1
-# 	j-=1
