161009b3e5f5a71a1d8f175c01fa87cb9dc7ef0e
Bharat Mazire <mazire.bharat@gmail.com>
2018-05-18 18:33:45 +0530

    Update frst_last_two_str.py

--- string/frst_last_two_str.py
+++ string/frst_last_two_str.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
-# problem statement : accept input string and create new strirng as first and last 2 eleme=nts of accepted strig.
+# problem statement : accept input string and create new strirng as first and last 2 elements of accepted strig.
+# eg : input string : hello
+#      output string: helo
 
 string = input("Enter the string as initial input : ")
-
 new_string = string[0:2]+string[-2:]
 print("old string was {}  and new is '{}'  ".format(string,new_string))
