9e4bb0aadee18bd4a699b019adf44afb135e9af3
Bharat Mazire <mazire.bharat@gmail.com>
2018-05-23 15:34:34 +0530

    Comments added to ColorSpiral.py

--- Turtle/ColorSpiral.py
+++ Turtle/ColorSpiral.py
@@ -1,15 +1,15 @@
 import turtle
 
-t = turtle.Pen()
-turtle.bgcolor("black")
+t = turtle.Pen()							# initialize pen
+turtle.bgcolor("black")							# setting background color to black
 
 # you can choose between 2 and 6 sides for some cool shapes !
 
-sides = 6
+sides = 6								# setting sides
 
-colors = ['red', 'yellow', 'blue', 'orange', 'green', 'purple']
-for x in range(360):
-	t.pencolor(colors[x%sides])
-	t.forward(x * 3/sides + x)
-	t.left(360/sides +1)
-	t.width(x*sides/200)
+colors = ['red', 'yellow', 'blue', 'orange', 'green', 'purple']		# listing some colors
+for x in range(360):							# looping
+	t.pencolor(colors[x%sides])					# selecting color from list
+	t.forward(x * 3/sides + x)					# forwarding pen to some distance
+	t.left(360/sides +1)						# moving pen head to some distance (here : 61)
+	t.width(x*sides/200)						# setting pen width to some amount
