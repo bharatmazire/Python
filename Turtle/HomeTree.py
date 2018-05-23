5f28d1a11f7ff54579f5b12266901757ae35e6af
Bharat Mazire <mazire.bharat@gmail.com>
2018-05-23 15:41:55 +0530

    Comments added to HomeTree.py

--- Turtle/HomeTree.py
+++ Turtle/HomeTree.py
@@ -5,8 +5,8 @@ t = turtle.Turtle()
 # Draw a brown house
 t.color('brown')
 
-# Draw the base
-for i in xrange(4):
+# Draw base of house
+for i in range(4):
     t.forward(50)
     t.left(90)
 
@@ -22,17 +22,24 @@ t.forward(50)
 t.right(120)
 t.forward(50)
 
+# going to draw tree
 t.right(30)
 t.forward(50)
 t.left(90)
-t.color('white')
+t.color('white')        # setting color to white so that we cant see 
+                        # we can either use penup() and pendown()
 
 t.forward(150)
+
+# drawing tree
 t.color('brown')
+
 
 t.left(90)
 t.forward(30)
+
 t.color('green')
+
 t.left(90)
 t.forward(35)
 t.right(130)
