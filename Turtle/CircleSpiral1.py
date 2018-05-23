4f12304d39363c71c1befe3ae00fcc69a4d901e9
Bharat Mazire <mazire.bharat@gmail.com>
2018-05-23 15:20:59 +0530

    comments added to CircleSpiral1.py

--- Turtle/CircleSpiral1.py
+++ Turtle/CircleSpiral1.py
@@ -1,7 +1,7 @@
 import turtle
 
-t = turtle.Pen()
+t = turtle.Pen()		# to initialize pen
 
-for x in range(100):
-	t.circle(x)
-	t.left(91)
+for x in range(100):		# loop 
+	t.circle(x)		# to draw cirle of readius x(value from 0 to 99 [loop value])
+	t.left(91)		# move pen direction to 91 degrees
