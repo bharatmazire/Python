0e08d174ae4138a345b7fa20fee99f34eb03555e
AM <forapple13363@gmail.com>
2020-07-30 11:22:49 +0100

    fixed divi_by_8 program

--- Programs/Basic/List/divi_by_8.py
+++ Programs/Basic/List/divi_by_8.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
 
-def divi_by_8():
-	
-
+def divi_by_8(x):
+    if x % 8 == 0:
+        print(x)
+    else:
+        pass
 
 def menu():
 	#lb , hb = accept()
 	low = eval(input("enter low no : "))
 	high = eval(input("enter high bound : "))
 
-	return [x for x in range (low , high ) if divi_by_8() ]
+	return [x for x in range (low , high ) if divi_by_8(x) ]
 
 if __name__ == '__main__':
-	print(menu())
+	menu()
